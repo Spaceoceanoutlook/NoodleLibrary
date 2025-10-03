aba5a514c033070ee689793923d25b184372ed38
Spaceoceanoutlook <spaceocean@outlook.com>
2025-10-03 12:05:19 +0500

    added all models

---
+++ noodlelibrary/models/country.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class Country(Base):
+    __tablename__ = "countries"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
--- noodlelibrary/models/noodle.py
+++ noodlelibrary/models/noodle.py
@@ -1,17 +1,23 @@
-# class Noodles:
-#     id: int
-#     title: str
-#     description: str
-#     recommendation: int
-#     country_id: int
-#     manufacture_id: int
-#     image: str
+from sqlalchemy import Boolean, ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from noodlelibrary.models.country import Country
+from noodlelibrary.models.manufacture import Manufacture
+
 from .base import Base
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
 
 
 class Noodle(Base):
     __tablename__ = "noodles"
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str | None] = mapped_column(String, nullable=True)
+    recommendation: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    country_id: Mapped[int] = mapped_column(ForeignKey("countries.id"), nullable=False)
+    manufacture_id: Mapped[int] = mapped_column(
+        ForeignKey("manufactures.id"), nullable=False
+    )
+    image: Mapped[str] = mapped_column(String, nullable=False)
+
+    country: Mapped["Country"] = relationship("Country", backref="noodles")
+    manufacture: Mapped["Manufacture"] = relationship("Manufacture", backref="noodles")
